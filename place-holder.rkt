18a15b3ebff87b3307e0a40cf50c6d787e5940ca
Vivian Zou <67518516+vivianzo@users.noreply.github.com>
2024-04-22 01:42:00 -0400

    Delete place-holder.rkt

--- place-holder.rkt
+++
@@ -1,9 +0,0 @@
-#lang htdp/isl+
-(define welcome-message
-  "Welocome! glad to see you here")
-
-(define place-holder 
-  "this is a placeholder")
-
-(define how-to-access
-  "please contact me to have access to the source code as per Northeastern Homework Policy")
