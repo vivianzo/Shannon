59b58c00ca740950f007887a382eb56a3b10b54c
Vivian Zou <67518516+vivianzo@users.noreply.github.com>
2024-04-21 23:47:16 -0400

    Add files via upload

---
+++ place-holder.rkt
@@ -0,0 +1,9 @@
+#lang htdp/isl+
+(define welcome-message
+  "Welocome! glad to see you here")
+
+(define place-holder 
+  "this is a placeholder")
+
+(define how-to-acess
+  "please contact me to have acess to the source code as per Northeastern Homework Policy")
